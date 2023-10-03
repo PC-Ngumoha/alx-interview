172e1fa95232c55b98930e655149806fae8729c4
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-03 16:07:59 +0100

    Update: Tentative solution for 'Lockboxes' mock interview question

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -19,6 +19,28 @@ def canUnlockAll(boxes):
   Return:
     - True -> All boxes can be unlocked.
     - False -> Not all boxes can be unlocked.
-  
+
+  Observation:
+    - I ended up making use of a Depth First Search Algorithm which
+      keeps track of boxes visited and uses that to determine if all boxes
+      can be unlocked or not.
+
+  TODO: I'm going to need to learn more about different algorithm formation
+  techniques.
   """
-  pass
+  n_boxes = len(boxes)
+  visited = set()  # Keeps track of boxes we've visited
+  key_stack = [0]
+
+  while key_stack:
+    current_box = key_stack.pop()
+
+    if current_box not in visited:
+      visited.add(current_box)
+      keys = boxes[current_box]
+
+      for key in keys:
+        if key >= 0 and key < n_boxes:
+          key_stack.append(key)
+
+  return n_boxes == len(visited)
