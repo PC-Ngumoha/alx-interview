7f114cb2d42343310c4464546ff55619b77f6478
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-03 11:10:48 +0100

    Setup: Setting up the files for the 'lockboxes' mock interview project

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+lockboxes (Mock Interview)
+
+My attempt at writing a function which takes a row of 
+boxes with boxes which may or may not contain keys to
+open subsequent boxes and then determine if all the boxes
+can be unlocked/opened
+"""
+
+
+def canUnlockAll(boxes):
+  """
+  canUnlockAll()
+
+  Args:
+    - boxes -> list of boxes
+
+  Return:
+    - True -> All boxes can be unlocked.
+    - False -> Not all boxes can be unlocked.
+  
+  """
+  pass
