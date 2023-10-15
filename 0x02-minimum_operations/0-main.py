3984e6d51a0c272268fde40c9b38104ca256d9f8
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-15 16:50:24 +0100

    Update: Fifth attempt at solving the algo problem

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -6,6 +6,6 @@ import time
 minOperations = __import__('0-minoperations').minOperations
 
 start = time.perf_counter()
-n = 1000
+n = 9
 print("Min # of operations to reach {} char: {} time: {} seconds".format(
   n, minOperations(n), time.perf_counter() - start))
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,14 @@
-# #!/usr/bin/python3
-# """
-# In this problem, we are given a character `H` in a file and a number `n` and
-# we are asked to create a function that calculates the minimum number of
-# operations `num_ops` required to get `n` number of `H` characters in the
-# file. A point to note is that we can only perform these two operations in the
-# file: `Copy All` and `Paste`
-# """
+#!/usr/bin/python3
+"""
+In this problem, we are given a character `H` in a file and a number `n` and
+we are asked to create a function that calculates the minimum number of
+operations `num_ops` required to get `n` number of `H` characters in the
+file. A point to note is that we can only perform these two operations in the
+file: `Copy All` and `Paste`
+"""
 
+
+# ITERATIVE APPROACH
 
 # def minOperations(n: int) -> int:
 #     """
@@ -31,20 +33,32 @@
 #                 all_ops[i] = min(all_ops[i], all_ops[j] + i // j)
 #     return all_ops[n]
 
-#!/usr/bin/python3
-'''
-minimum operations solution algorithm
-'''
-from math import floor
 
+# RECURSIVE APPROACH
 
-def minOperations(n, ops=0):
-    '''
-    number of operations required to get n Hs
-    '''
-    if (n <= 1):
+def minOperations(n: int, memo: dict = {}) -> int:
+    """
+    minOperations(n)
+
+    Args:
+        - n (int) -> number of H characters to create
+        - memo (dict) -> a cache to store the values from previous recursions
+
+    Returns:
+        - num_ops (int) -> number of operations required to create n H
+        characters
+    """
+    if n in memo:
+        return memo[n]
+
+    if n <= 1:
         return 0
-    for i in range(2, (n // 2) + 1)[::-1]:
+
+    min_ops = n 
+
+    for i in range(2, n // 2 + 1):
         if n % i == 0:
-            return minOperations(i, ops + (n / i))
-    return int(ops + n)
+            min_ops = min(min_ops, minOperations(i, memo) + n // i)
+
+    memo[n] = min_ops
+    return min_ops
