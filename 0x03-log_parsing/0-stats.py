11fabd1ea6a2ddd0e731da9e46b6cc30512a1ef7
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-20 01:30:31 +0100

    Update: Third attempt at correcting the algo

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -17,19 +17,17 @@ if __name__ == '__main__':
     pattern2 = r'\b[1-5]\d{2} \d+$'
     count = 0
     total_size = 0
-    s_codes = {}
+    s_codes = {"200": 0, "301": 0, "400": 0,
+                  "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     try:
         for line in sys.stdin:
             if not line:
                 break
             if count % 10 == 0 and count != 0:
-                print('File Size: {}'.format(total_size))
+                print('File size: {}'.format(total_size))
                 for s_code in s_codes:
-                    print('{}: {}'.format(
-                        s_code,
-                        s_codes[s_code]
-                    ))
-
+                    if s_codes[s_code] > 0:
+                        print('{}: {}'.format(s_code, s_codes[s_code]))
             line_match = re.fullmatch(pattern1, line.rstrip())
             if not line_match:
                 continue
@@ -37,15 +35,10 @@ if __name__ == '__main__':
                 segment_match = re.search(pattern2, line.rstrip())
                 s_code, file_size = segment_match.group().split(' ')
                 total_size += int(file_size)
-                if s_code not in s_codes:
-                    s_codes[s_code] = 1
-                else:
-                    s_codes[s_code] += 1
+                s_codes[s_code] += 1
             count += 1
     except KeyboardInterrupt:
-        print('File Size: {}'.format(total_size))
+        print('File size: {}'.format(total_size))
         for s_code in s_codes:
-            print('{}: {}'.format(
-                s_code,
-                s_codes[s_code]
-            ))
+            if s_codes[s_code] > 0:
+                print('{}: {}'.format(s_code, s_codes[s_code]))
