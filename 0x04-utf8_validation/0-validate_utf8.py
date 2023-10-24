688d266c2d05a95b4f8e6135ef71576deb813b5f
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-24 09:49:29 +0100

    Setup: setup the project directory and the starting files for the project/interview

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+0-validate_utf8.py
+
+Contains the definition of the validUTF8() function which takes
+data set containing multiple characters and determines whether the
+data set represents a valid UTF8 encoding.
+"""
+from typing import List
+
+
+def validUTF8(data: List[int]) -> bool:
+    """
+    validUTF8(data)
+
+    Args:
+      - data (list[int]) -> data set to validate.
+
+    Returns:
+      - A bool representing whether it's valid or not.
+    """
+    pass
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,6 @@
+# UTF-8 Validation (Mock Interview)
+
+### Objective:
+The objective of this mock interview question is to challenge you to write a method that determines if a given data set represents a valid UTF-8 encoding.
+
+### My Thought Process
