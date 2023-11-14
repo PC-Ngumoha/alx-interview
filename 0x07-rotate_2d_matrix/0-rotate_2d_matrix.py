0bd44a2777356e5c3ae3e2dc9f70c89070e46acb
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-14 21:46:24 +0100

    Initial attempt at solving the algo

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -17,4 +17,12 @@ def rotate_2d_matrix(matrix):
     Returns:
       - Nothing. Rotates the matrix in place
     """
-    pass
+    matrix_size = len(matrix)
+    last_row_index = matrix_size - 1
+    temp_matrix = []
+    for j in range(0, matrix_size):
+        rotated_row = []
+        for i in range(last_row_index, -1, -1):
+            rotated_row.append(matrix[i][j])
+        temp_matrix.append(rotated_row)
+    matrix[:] = temp_matrix
--- 0x07-rotate_2d_matrix/main_0.py
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
+    # matrix = [[1, 2],
+    #           [4, 5],
+    #           [7, 8]]
 
     rotate_2d_matrix(matrix)
     print(matrix)
