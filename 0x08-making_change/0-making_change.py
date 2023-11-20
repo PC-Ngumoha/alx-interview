47c6e6ea40918c5cadeaa46b82e417625ccf501f
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-20 16:32:59 +0100

    Setup: Create project folder for the 'Making Change' algo project

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+0-making_change.py
+
+Contains the definition of the makeChange function which determines
+the minimum number of coins in your wallet are required to meet a
+a given amount
+"""
+from typing import List
+
+
+def makeChange(coins: List[int], total: int) -> int:
+    """Determines minimum number of coins to use as change for total.
+
+    Parameter:
+      - coins: The denominations of coins you have to make change.
+      - total: The amount/denomination your coin combination has to meet.
+
+    Returns:
+      - change: The count of coins needed to meet total or -1 if not possible.
+    """
+    pass
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+# Making Change (Mock Interview)
+
+## My Thought Process
