91c8fb4c51b76fea341a136e27010ed689a44ac8
PC-Ngumoha <pcngumoha@gmail.com>
2023-11-28 10:55:56 +0100

    Update: Experimenting

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,30 +13,29 @@ def island_perimeter(grid):
     Returns:
       - perimeter of the island.
     """
+    def get_perimeter(i, j):
+        """ get_perimeter()
+
+        Parameters:
+          - i: current row being evaluated
+          - j: current column being evaluated
+
+        Returns:
+          - 1, 0 or sum of perimeter of the current cell under consideration.
+        """
+        if (i < 0 or i > len(grid) - 1) or (j < 0 or j > len(grid[i]) - 1):
+            return 1
+        if grid[i][j] == 0:
+            return 1
+        if grid[i][j] == 'v':
+            return 0
+        grid[i][j] = 'v'
+        return (get_perimeter(i, j-1) + get_perimeter(i-1, j)
+                + get_perimeter(i, j+1) + get_perimeter(i+1, j))
+
     result = 0
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                result = get_perimeter(row, col, grid)
+                result = get_perimeter(row, col)
     return result
-
-
-def get_perimeter(i, j, grid):
-    """ get_perimeter()
-
-    Parameters:
-      - i: current row being evaluated
-      - j: current column being evaluated
-
-    Returns:
-      - 1, 0 or sum of perimeter of the current cell under consideration.
-    """
-    if (i < 0 or i > len(grid) - 1) or (j < 0 or j > len(grid[i]) - 1):
-        return 1
-    if grid[i][j] == 0:
-        return 1
-    if grid[i][j] == 'v':
-        return 0
-    grid[i][j] = 'v'
-    return (get_perimeter(i, j-1, grid) + get_perimeter(i-1, j, grid)
-            + get_perimeter(i, j+1, grid) + get_perimeter(i+1, j, grid))
