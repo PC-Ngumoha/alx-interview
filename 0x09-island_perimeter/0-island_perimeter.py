e4be22daaf02b2bd0999bd3362b53d6482218a8c
PC-Ngumoha <pcngumoha@gmail.com>
2023-12-01 17:07:53 +0100

    Update: Fixed PEP8 issue

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -30,8 +30,8 @@ def island_perimeter(grid):
         if grid[i][j] == 'v':
             return 0
         grid[i][j] = 'v'
-        return (get_perimeter(i, j-1) + get_perimeter(i-1, j)
-                + get_perimeter(i, j+1) + get_perimeter(i+1, j))
+        return (get_perimeter(i, j-1) + get_perimeter(i-1, j) +
+                get_perimeter(i, j+1) + get_perimeter(i+1, j))
 
     result = 0
     if type(grid) != list:
@@ -41,25 +41,3 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 result = get_perimeter(row, col)
     return result
-
-# NOTE: This is not my code
-# def island_perimeter(grid):
-#     """Computes the perimeter of an island with no lakes.
-#     """
-#     perimeter = 0
-#     if type(grid) != list:
-#         return 0
-#     n = len(grid)
-#     for i, row in enumerate(grid):
-#         m = len(row)
-#         for j, cell in enumerate(row):
-#             if cell == 0:
-#                 continue
-#             edges = (
-#                 i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
-#                 j == m - 1 or (m > j + 1 and row[j + 1] == 0),
-#                 i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
-#                 j == 0 or row[j - 1] == 0,
-#             )
-#             perimeter += sum(edges)
-#     return perimeter
