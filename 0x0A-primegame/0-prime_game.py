43cef095f26ad8a316b4f4fd056bc087e8aab85d
PC-Ngumoha <pcngumoha@gmail.com>
2023-12-04 13:08:43 +0100

    Setup: Setting up the project directory

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""0-prime_game.py
+My attempt at implementing the 'Prime Game' Algorithm 
+"""
+
+
+def isWinner(x, nums):
+    """isWinner()
+
+    Parameters:
+      - x: number of rounds
+      - nums: set/list of random prime numbers
+
+    Returns:
+      - Who won between 'Maria' and 'Ben' 
+    """
+    pass
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,1 @@
+# Prime Game (Mock Interview)
